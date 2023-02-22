ab8e9bc4554abb4134d62279abae2a8c388aca7f
Yoongi Kim <yoongi@yoongi.kim>
2023-02-22 16:08:21 +0900

    업로드

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,84 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="grey-darken-3"
+      dark
+    >
+      <v-app-bar-title>Text index calculator</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>
+                길이: {{text.length}}
+              </v-card-title>
+              <v-card-title>
+                선택영역 (start <= n < end)
+                <br/>
+                {{selection.start}}:{{selection.end}}
+              </v-card-title>
+              <v-card-text>
+                <v-textarea
+                  v-model="text"
+                  label="텍스트"
+                  outlined
+                  dense
+                  rows="30"
+                  @mousedown="handleMouseDown"
+                  @mouseup="handleMouseUp"
+                  @mousemove="handleMouseMove"
+                />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+      <!-- copyright Yoongi Kim (yoongi@bhsn.ai)-->
+      <v-footer
+        color="grey-darken-3"
+        dark
+        app
+      >
+        <v-spacer/>
+        <span class="white--text text-right">&copy; Made by Yoongi Kim (yoongi@bhsn.ai)</span>
+      </v-footer>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ipsa, fugit a reprehenderit mollitia voluptatum omnis magnam debitis eos nostrum repudiandae laudantium accusantium odit exercitationem nulla sunt placeat nihil! Quaerat!',
+    selection: {start: 0, end: 0},
+    isMouseDown: false,
+  }),
+  methods: {
+    handleMouseDown() {
+      this.isMouseDown = true;
+    },
+    handleMouseUp() {
+      this.isMouseDown = false;
+    },
+    handleMouseMove(event) {
+      if (this.isMouseDown) {
+        const { selectionStart, selectionEnd } = event.target;
+        this.selection = { start: selectionStart, end: selectionEnd };
+      }
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+textarea {
+  font-family: monospace;
+  text-wrap: pre;
 }
 </style>
